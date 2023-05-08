db0184faaf3251a0f57bc4c9c3a7d9b96237e50d
Muukii <muukii.app@gmail.com>
2023-05-08 23:46:05 +0900

    Patch

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
-      name: "swiftui-color",
-      targets: ["swiftui-color"]),
+      name: "SwiftUIColor",
+      targets: ["SwiftUIColor"]),
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
@@ -23,10 +23,10 @@ let package = Package(
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
-      name: "swiftui-color",
+      name: "SwiftUIColor",
       dependencies: []),
     .testTarget(
-      name: "swiftui-colorTests",
-      dependencies: ["swiftui-color"]),
+      name: "SwiftUIColorTests",
+      dependencies: ["SwiftUIColor"]),
   ]
 )
